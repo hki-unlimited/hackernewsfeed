5fc530d76c499201014b14bcbceca080bc6b644f
Vesa Tahkavuori <vesatahkavuori@Vesas-Air.home>
2019-10-12 13:15:22 +0300

    added more comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HackerNewsList from './components/HackerNewsList/HackerNewsList';
 import HackerNewsAPI from './api/HackerNewsAPI';
@@ -9,11 +8,13 @@ function App() {
   const [topStories, setTopStories] = useState<HackerNewsItem[]>([]);
 
   useEffect(() => {
+    //fetch top stories
     HackerNewsAPI.fetchTopStories().then((topStoriesIdList: number[]) => {
+      //get the first ten stories, then get the details for each of story.
       Promise.all(topStoriesIdList.slice(0,10).map((id: number) => {
-        return HackerNewsAPI.fetchTopStoryDetails(id);
+        return HackerNewsAPI.fetchTopStoryDetails(id); //fetch the details: title, url etc
       })).then((topStoriesWithDetails: HackerNewsItem[]) => {
-        setTopStories(topStoriesWithDetails);
+        setTopStories(topStoriesWithDetails); //set state variale which will be passed to child component
       });
     });
   }, []);
